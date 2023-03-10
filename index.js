98a4581067ab5d035861602d0abbc3fffdb800cc
Gustavo Marialva <gmarialva@gmail.com>
2023-03-10 15:54:00 -0300

    corrige informações mobile

--- index.js
+++ index.js
@@ -14,17 +14,45 @@ perguntas.forEach((element) => {
   });
 });
 
-const accordion = document.querySelector('[data-js="accordion"]');
+// const accordion = document.querySelector('[data-js="accordion"]');
 
-accordion.addEventListener("click", (e) => {
-  const accordionHeaderId = e.target.dataset.accordionHeader;
-  const clickedAccordionHeader = document.querySelector(
-    `[data-accordion-header = "${accordionHeaderId}"]`
-  );
-  const accordionItemToBeOpened = document.querySelector(
-    `[data-accordion-body = "${accordionHeaderId}"]`
-  );
+// accordion.addEventListener("click", (e) => {
+//   const accordionHeaderId = e.target.dataset.accordionHeader;
+//   const clickedAccordionHeader = document.querySelector(
+//     `[data-accordion-header = "${accordionHeaderId}"]`
+//   );
+//   const accordionItemToBeOpened = document.querySelector(
+//     `[data-accordion-body = "${accordionHeaderId}"]`
+//   );
 
-  clickedAccordionHeader.classList.toggle("active");
-  accordionItemToBeOpened.classList.toggle("active");
+//   clickedAccordionHeader.classList.toggle("active");
+//   accordionItemToBeOpened.classList.toggle("active");
+// });
+
+const acc_btns = document.querySelectorAll(".accordion-header");
+const acc_contents = document.querySelectorAll(".accordion-body");
+
+acc_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    acc_contents.forEach((acc) => {
+      if (
+        e.target.nextElementSibling !== acc &&
+        acc.classList.contains("active")
+      ) {
+        acc.classList.remove("active");
+        acc_btns.forEach((btn) => btn.classList.remove("active"));
+      }
+    });
+
+    const panel = btn.nextElementSibling;
+    panel.classList.toggle("active");
+    btn.classList.toggle("active");
+  });
 });
+
+window.onclick = (e) => {
+  if (!e.target.matches(".accordion-header")) {
+    acc_btns.forEach((btn) => btn.classList.remove("active"));
+    acc_contents.forEach((acc) => acc.classList.remove("active"));
+  }
+};
