af30fb492f957736d0e3ebd13588bb64eed9700d
Gustavo Marialva <gmarialva@gmail.com>
2023-03-17 10:31:32 -0300

    cria function validaPrimeiroDigito

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,6 +1,7 @@
 export default function ehUmCPF(campo) {
   const cpf = campo.value.replace(/\.|-/g, "");
   validaNumerosRepetidos(cpf);
+  validaPrimeiroDigito(cpf);
 
   console.log(validaNumerosRepetidos(cpf));
 }
@@ -22,3 +23,25 @@ function validaNumerosRepetidos(cpf) {
 
   return numerosRepetidos.includes(cpf);
 }
+
+function validaPrimeiroDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 10;
+
+  for (let tamanho = 0; tamanho < 9; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 1) {
+    soma = 0;
+  }
+
+  return soma != cpf[9];
+}
+
+// no interior da função, criaremos um laço de repetição for que possuirá a variável let tamanho = 0, a qual determinaremos que deve se repetir nove vezes (para os 9 primeiros dígitos do CPF) através do comando tamanho < 9; tamanho++. Em seguida adicionaremos no interior das chaves deste for a variável soma que declaramos anteriormente e que agora deverá recolher os valores do CPF de acordo com cada posição do for. Em seguida vamos multiplicá-lo pelo valor de multiplicador. Na linha de baixo adicionaremos o comando multiplicador-- que completará o loop duplo no qual o CPF aumenta a posição do tamanho enquanto o multiplicador a diminui.
+// Este trecho de código retornará o final da multiplicação de cada dígito do CPF e também a soma total dessas multiplicações na variável soma. Abaixo do for vamos adicionar o valor final de soma, multiplicá-lo por 10 e dividi-lo por 11. Em seguida adicionaremos a condicional if para transformar em 0 os valores que resultarem em 10 ou 11. Também será adicionado um return comparando o valor final de soma com a posição 9 do CPF.
+// exemplo: se utilizarmos o CPF "937.777.040-83" como base, a soma devolvida pelo for será 311. Portanto o resultado da operação 311 = (311 * 10) % 11; nos retornará o resultado 8. Neste caso, nosso CPF não entrará no if e será devolvido o valor 8.
